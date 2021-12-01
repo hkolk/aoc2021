6540c87c8cc20c41a483aced1e0eebb6381cc51f
Harm Kolk <hkolk@ebay.com>
2021-12-01 20:39:05 +0100

    Cleanup number 1

--- src/main/kotlin/nl/hkolk/aoc2020/Day1.kt
+++ src/main/kotlin/nl/hkolk/aoc2020/Day1.kt
@@ -3,35 +3,17 @@ package nl.hkolk.aoc2020
 class Day1(input: List<String>) {
     private val input = input.map { it.toInt() }
 
-    fun solvePart1Old(): Int {
+    fun solve(windowsize:Int = 1): Int {
         var increments = 0
-        var prev = 0
-        for(x in input) {
-            if(x > prev && prev != 0) {
-               increments++
-            }
-            prev = x
-        }
-        return increments
-    }
-    fun solvePart1(): Int {
-        var increments = 0
-        input.windowed(2) {
-            if(it[1] > it[0]) {
-                increments++
-            }
-        }
-        return increments
-    }
-    fun solvePart2(): Int {
-        var increments = 0
-        input.windowed(4) {
-            val prev = it.take(3).sum()
-            val next = it.drop(1).take(3).sum()
+        input.windowed(windowsize+1) {
+            val prev = it.take(windowsize).sum()
+            val next = it.drop(1).take(windowsize).sum()
             if(next > prev) {
                 increments++
             }
         }
         return increments
     }
+    fun solvePart1(): Int = solve(1)
+    fun solvePart2(): Int = solve(3)
 }
