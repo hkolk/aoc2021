231f738f0189ff5ff46900999dcbea732f5fac8b
Harm Kolk <hkolk@ebay.com>
2021-12-03 07:23:21 +0100

    Day3 fing ugly...

---
+++ src/main/kotlin/nl/hkolk/aoc2020/Day3.kt
@@ -0,0 +1,69 @@
+package nl.hkolk.aoc2020
+
+class Day3(input: List<String>) {
+    private val length = input[0].length
+    private val input = input.map { it.toInt(2) }
+
+    fun solvePart1(): Int {
+        var res = IntArray(length) { 0 }
+        for(line in input) {
+            for(i in length-1 downTo 0) {
+                if(line and (1 shl i) > 0) {
+                    res[i] += 1
+                }
+            }
+        }
+        var gamma = 0
+        var epsilon = 0
+        for(i in 0 until length) {
+            if(res[i] > input.size/2) {
+                gamma += (1 shl i)
+            } else {
+                epsilon += (1 shl i)
+            }
+        }
+        return gamma * epsilon
+    }
+
+    fun solvePart2(): Int {
+        var remaining = input
+        for(i in (length-1) downTo 0) {
+            var accu = 0
+            for(line in remaining) {
+                if(line and (1 shl i) > 0) {
+                    accu += 1
+                }
+            }
+            remaining = if(accu.toDouble() >= (remaining.size/2.0)) {
+                remaining.filter { it and (1 shl i) > 0 }
+            } else {
+                remaining.filter { it.inv() and (1 shl i) > 0 }
+            }
+            if(remaining.size == 1) {
+                break;
+            }
+
+        }
+        var oxygen = remaining[0]
+        remaining = input
+        for(i in (length-1) downTo 0) {
+            var accu = 0
+            for(line in remaining) {
+                if(line and (1 shl i) > 0) {
+                    accu += 1
+                }
+            }
+            remaining = if(accu.toDouble() < (remaining.size/2.0)) {
+                remaining.filter { it and (1 shl i) > 0 }
+            } else {
+                remaining.filter { it.inv() and (1 shl i) > 0 }
+            }
+            if(remaining.size == 1) {
+                break;
+            }
+        }
+        var co2 = remaining[0]
+        return co2*oxygen
+
+    }
+}
---
+++ src/test/kotlin/nl/hkolk/aoc2020/Day3Test.kt
@@ -0,0 +1,58 @@
+package nl.hkolk.aoc2020
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import java.util.*
+
+
+@DisplayName("Day 3")
+class Day3Test {
+
+    val testInput =
+        """
+            00100
+            11110
+            10110
+            10111
+            10101
+            01111
+            00111
+            11100
+            10000
+            11001
+            00010
+            01010
+        """.trimIndent().splitIgnoreEmpty("\n")
+    val realInput = Resources.resourceAsList("day3.txt")
+
+    @Nested
+    @DisplayName("Part 1")
+    inner class Part1 {
+        @Test
+        fun `Matches Example`() {
+            val answer = Day3(testInput).solvePart1()
+            assertThat(answer).isEqualTo(198)
+        }
+        @Test
+        fun `Actual Answer`() {
+            val answer = Day3(realInput).solvePart1()
+            assertThat(answer).isEqualTo(3885894)
+        }
+    }
+    @Nested
+    @DisplayName("Part 2")
+    inner class Part2 {
+        @Test
+        fun `Matches Example`() {
+            val answer = Day3(testInput).solvePart2()
+            assertThat(answer).isEqualTo(230)
+        }
+        @Test
+        fun `Actual Answer`() {
+            val answer = Day3(realInput).solvePart2()
+            assertThat(answer).isEqualTo(2073416724)
+        }
+    }
+}
