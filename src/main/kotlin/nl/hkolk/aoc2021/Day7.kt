c1798af277a38759ef6baf91dc07ba7ba8f4a3b5
Harm Kolk <hkolk@ebay.com>
2021-12-07 08:02:02 +0100

    Day7 reduce readability

--- src/main/kotlin/nl/hkolk/aoc2021/Day7.kt
+++ src/main/kotlin/nl/hkolk/aoc2021/Day7.kt
@@ -5,15 +5,12 @@ import kotlin.math.absoluteValue
 class Day7(input: List<String>) {
     private val positions = input[0].splitIgnoreEmpty(",").map { it.toInt() }
 
-    fun solve(fuelrate: (Int, Int) -> Int): Int {
-        val min = positions.minOrNull()?:0
-        val max = positions.maxOrNull()?:0
-        val distances = (min..max).map { pos -> pos to
-                positions.fold(0) { sum, element ->
-                    sum + fuelrate(element, pos)
-                }
+    private fun solve(fuelrate: (Int, Int) -> Int): Int {
+        return (positions.minOrNull()!!..positions.maxOrNull()!!).fold(Int.MAX_VALUE){ lowest, pos ->
+            minOf(lowest, positions.fold(0) { sum, element ->
+                sum + fuelrate(element, pos)
+            })
         }
-        return distances.map { it.second }.minOrNull()?:0
     }
     fun solvePart1(): Int = solve { element, pos -> (element - pos).absoluteValue }
     fun solvePart2(): Int = solve { element, pos -> ((element - pos).absoluteValue downTo 0).sum() }
