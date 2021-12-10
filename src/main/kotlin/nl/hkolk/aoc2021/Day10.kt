9cb15a3e0df7316f28380886b8f855385f46c745
Harm Kolk <hkolk@ebay.com>
2021-12-10 09:45:10 +0100

    Day10 cleanups

--- src/main/kotlin/nl/hkolk/aoc2021/Day10.kt
+++ src/main/kotlin/nl/hkolk/aoc2021/Day10.kt
@@ -2,53 +2,36 @@ package nl.hkolk.aoc2021
 
 class Day10(val input:List<String>) {
 
-    fun solvePart1(): Int {
-        var score = 0
-        for(line in input) {
-            var line = line
-            while (true) {
-                val newline = line.replace("[]", "").replace("()", "").replace("<>", "").replace("{}", "")
-                if (newline.length == line.length) {
-                    break
-                }
-                line = newline
+    private fun reduce(originalLine: String) : String {
+        var line = originalLine
+        while (true) {
+            val newline = line.replace("[]", "").replace("()", "").replace("<>", "").replace("{}", "")
+            if (newline.length == line.length) {
+                break
             }
-            println("   ${input[0]}")
-            println("-> $line")
-            if(line.contains("""[\]\>\}\)]""".toRegex())) {
-                println("Corrupt!")
-                val match = """[\]\>\}\)]""".toRegex().find(line)
-                println(match?.value)
-                score += when (match?.value) {
-                    ")" -> 3
-                    "]" -> 57
-                    "}" -> 1197
-                    ">" -> 25137
-                    else -> 0
-                }
-
-                //val first = "}>)]".toCharArray().map { line.indexOf(it) }.minOrNull()
-            } else {
-                println("Incomplete!")
-            }
+            line = newline
         }
-        return score
+        return line
     }
-    fun solvePart2(): Long {
-        var scores = mutableListOf<Long>()
-        for(line in input) {
-            var line = line
-            while (true) {
-                val newline = line.replace("[]", "").replace("()", "").replace("<>", "").replace("{}", "")
-                if (newline.length == line.length) {
-                    break
-                }
-                line = newline
+
+    fun solvePart1(): Int =
+        input.map {
+            val match = """[]>})]""".toRegex().find(reduce(it))
+            when (match?.value) {
+                ")" -> 3
+                "]" -> 57
+                "}" -> 1197
+                ">" -> 25137
+                else -> 0
             }
-            if(!line.contains("""[\]\>\}\)]""".toRegex())) {
-                println("   ${input[0]}")
-                println("-> $line")
-                println("Incomplete!")
+        }.sum()
+
+
+    fun solvePart2(): Long {
+        val scores = mutableListOf<Long>()
+        for(originalLine in input) {
+            val line = reduce(originalLine)
+            if(!line.contains("""[]>})]""".toRegex())) {
                 val score = line.reversed().map { when(it) {
                     '(' -> 1
                     '[' -> 2
@@ -56,10 +39,9 @@ class Day10(val input:List<String>) {
                     '<' -> 4
                     else -> 0
                 } }.fold(0L) {accu, value -> accu * 5 + value}
-                println(score)
                 scores.add(score)
             }
         }
-        return scores.sorted().get(scores.size/2)
+        return scores.sorted()[scores.size/2]
     }
 }
